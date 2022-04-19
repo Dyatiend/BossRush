d66d972cba7369eb5c0da7197cee831f9ac3aacf
Moskalenko Nikita <53981215+Strupervlg@users.noreply.github.com>
2022-04-19 10:14:17 +0300

    Added grenade explosion

--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -7,6 +7,10 @@ public class Grenade : MonoBehaviour
 
     public float delay = 3f;
 
+    public GameObject explosionEffect;
+
+    public float radius = 3f;
+
     float countdown;
     bool hasExploded = false;
 
@@ -27,7 +31,19 @@ public class Grenade : MonoBehaviour
     }
 
     void Explode() {
-        Debug.Log("Boom");
+        GameObject explosion = Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Destroy(explosion, 1f);
+
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider collider in colliders) {
+            if(collider.gameObject.CompareTag("Tyan")) {
+                Debug.Log("Tyan razorvalo");
+            }
+        }
+        
         Destroy(gameObject);
     }
 }
