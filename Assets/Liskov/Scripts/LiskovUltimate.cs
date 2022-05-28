4ab4736e3f50cc5ec846d38d0ab62bf61b5219e9
etoyulich <gomazkova19@yandex.ru>
2022-05-28 19:23:54 +0300

    Лисков готова

---
+++ Assets/Liskov/Scripts/LiskovUltimate.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiskovUltimate : Skill
+{
+    // Start is called before the first frame update
+    public override SkillType Type()
+    {
+        return SkillType.Ultimate;
+    }
+
+    public override bool NeedsMouseRotation()
+    {
+        return false;
+    }
+
+    protected override float HoldUpTime()
+    {
+        return 4.4f;
+    }
+
+    protected override float ActiveTime()
+    {
+        return 4.4f;
+    }
+
+    protected override float ReloadTime()
+    {
+        return 2;
+    }
+
+    public int playerDamage = 20;
+    public int bossDamage = 25;
+    protected override void Action()
+    {
+        gameObject.GetComponent<Health>().TakeDamage(10);
+        Targeting targeting = gameObject.GetComponent<Targeting>();
+        targeting.ConfigureTargetingAs(gameObject.tag);
+        if (CompareTag("Player"))
+        {
+            GameObject.FindWithTag(targeting.TargetTag).GetComponent<Health>().TakeDamage(playerDamage);
+        }
+        else
+        {
+            GameObject.FindWithTag(targeting.TargetTag).GetComponent<Health>().TakeDamage(bossDamage);
+        }
+        
+    }
+    
+}
